95c1f34ab437999d98666a1f34c66c76cb16ac0a
mohaiminul69 <mohaiminul69@gmail.com>
2021-12-26 12:58:43 +0600

    component added

---
+++ src/Components/Example/Example.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Example = () => {
+  return (
+    <div>
+      <h1>Create a folder and inside that a file and then do all your work</h1>
+    </div>
+  );
+};
+
+export default Example;
