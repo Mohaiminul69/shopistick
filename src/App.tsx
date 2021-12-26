6932e61077b17f652eaeae9dc63d4cadbd64060b
mohaiminul69 <mohaiminul69@gmail.com>
2021-12-26 21:28:25 +0600

    branch created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Example from './Components/Example/Example';
+import Shop from './Components/Shop/Shop';
 
 function App() {
   return (
       <Routes>
         <Route path="/example" element={<Example />} />
+        <Route path="/shop" element={<Shop />} />
       </Routes>
   );
 }
