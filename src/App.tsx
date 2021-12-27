f126aa4cd68464f33fe483436554f2fb3431df88
rahatkabir007 <rahatkabir04@gmail.com>
2021-12-27 20:44:09 +0600

    login-register-contact added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
+import ContactUs from './Components/ContactUs/ContactUs';
 import Example from './Components/Example/Example';
+import LoginRegister from './Components/LoginRegister/LoginRegister';
 
 function App() {
   return (
       <Routes>
         <Route path="/example" element={<Example />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/loginRegister" element={<LoginRegister />} />
       </Routes>
   );
 }
